bbc40f4a1411cec8c20769860a1da73a64a43fd6
ehurturk <emirhurturk444@gmail.com>
2021-01-30 00:34:22 +0300

    Updated

--- regression.py
+++ regression.py
@@ -1,8 +1,26 @@
 import random
+import numpy as np
+
+
+class Regression:
+    def __init__(self, inputs):
+        self.weights = np.random.rand(1, len(inputs))
+        print(self.weights)
+
+    def predict(self, input_):
+        '''
+            Returns the dot product of inputs and paramerers
+            p(xj) = w0x0 + w1x1 + w2x2 + ... + wNxN in which xj = a training ex.
+                                                             xji = i'th feature of the j'th training example
+        '''
+        prediction = 0
+        for i in range(0, len(self.weights)):
+            prediction += self.weights[i] * input_
+        return prediction
 
 class LinearRegression(Regression):
     def __init__(self, inputs):
-        self,inputs = inputs
+        self.inputs = inputs
         super().__init__(inputs)
     def cost(self, true_labels):
         '''
@@ -16,16 +34,4 @@ class LinearRegression(Regression):
     def gradient_descent(self, epochs):
         return NotImplementedError
 
-class Regression:
-    def __init__(self, inputs):
-        self.weights = [random.uniform(0, 5) for _ in inputs]
-    def predict(self, input_):
-        '''
-            Returns the dot product of inputs and paramerers
-            p(xj) = w0x0 + w1x1 + w2x2 + ... + wNxN in which xj = a training ex.
-                                                             xji = i'th feature of the j'th training example
-        '''
-        prediction = 0
-        for i in range(0, len(self.weights)):
-            prediction += self.weights[i] * input_
-        return prediction
+
