fddd85954d7fe82a730a85ee1c6469dbac52c6a7
Emir Hurturk <emirhurturk444@gmail.com>
2021-01-29 11:48:23 +0300

    Updated

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from regression import LinearRegression
 
 if __name__ == "__main__":
     sd = StockData()
-    cv = sd.get_stocks('TSLA', 'TIME_SERIES_DAILY_ADJUSTED', 10)
+    stock_data = sd.get_stocks('TSLA', 'TIME_SERIES_DAILY_ADJUSTED', 10)
+    lr = LinearRegression()
 
